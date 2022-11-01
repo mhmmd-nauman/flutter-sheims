dc249651d722cfd274e279cc955d64f798d1dd4f
ahteshamweb <107624205+ahteshamweb@users.noreply.github.com>
2022-11-01 20:37:46 +0500

    Update fee_data.dart

--- myiub/lib/screens/fee_screen/data/fee_data.dart
+++ myiub/lib/screens/fee_screen/data/fee_data.dart
@@ -11,7 +11,7 @@ class FeeData {
 }
 
 List<FeeData> fee = [
-  FeeData('90871', 'November', '8 Nov 2020', 'Pending', '980\$', 'PAY NOW'),
-  FeeData('90870', 'September', '8 Sep 2020', 'Paid', '1080\$', 'DOWNLOAD'),
-  FeeData('908869', 'August', '8 Aug 2020', 'Paid', '950\$', 'DOWNLOAD'),
+  FeeData('90871', 'November', '8 Nov 2020', 'Pending', 'Rs.40,000', 'PAY NOW'),
+  FeeData('90870', 'September', '8 Sep 2020', 'Paid', 'Rs.40,000', 'DOWNLOAD'),
+  FeeData('908869', 'August', '8 Aug 2020', 'Paid', 'Rs.40,000' 'DOWNLOAD'),
 ];
